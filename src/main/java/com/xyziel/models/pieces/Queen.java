df44c77321d84e2956b8d7297b7721606bd1bd53
Radosław Bujak <radobujak@wp.pl>
2020-11-05 22:23:46 +0100

    fix queen movement

--- src/main/java/com/xyziel/models/pieces/Queen.java
+++ src/main/java/com/xyziel/models/pieces/Queen.java
@@ -27,7 +27,7 @@ public class Queen extends Piece {
             /*moves to the bottom-left*/
             7, 14, 21, 28, 35, 42, 49,
             /*moves to the bottom-right*/
-            9, 18, 27, 36, 45, 54, 63 };
+            9, 18, 27, 36, 45, 54, 63};
 
     public Queen(Color color) {
         try {
@@ -47,17 +47,31 @@ public class Queen extends Piece {
     public ArrayList<Integer> getPossibleMoves(int position, Board board) {
         ArrayList<Integer> possibleMoves = new ArrayList<>();
         Tile[] tiles = board.getTiles();
-        for(int i = 0; i < moves.length; i++) {
-            if((moves[i] + position >= 0) && (moves[i] + position <= 63)) {
-                if ((tiles[moves[i] + position].getPiece() != null)) {
-                    if(tiles[moves[i] + position].getPiece().getPieceColor() != this.pieceColor) {
+        int row = position / 8;
+        for (int i = 0; i < moves.length; i++) {
+            if ((moves[i] + position >= 0) && (moves[i] + position <= 63)) {
+                if (i < 28 && ((moves[i] + position) / 8 == row || moves[i] % 8 == 0)) {
+                    if ((tiles[moves[i] + position].getPiece() != null)) {
+                        if (tiles[moves[i] + position].getPiece().getPieceColor() != this.pieceColor) {
+                            possibleMoves.add(moves[i] + position);
+                        }
+                        i = i / 7 * 7 + 6;
+                    } else {
                         possibleMoves.add(moves[i] + position);
                     }
-                    i = i / 7 * 7 + 6;
-                } else {
-                    possibleMoves.add(moves[i] + position);
-                    if((moves[i]+position) % 8 == 0 || (moves[i]+position+1) % 8 == 0) {
-                        i = i/7 * 7 + 6;
+                }
+                if (i > 27) {
+                    if (((i / 7 == 4 || i / 7 == 6) && (moves[i] + position) % 8 != 7) || ((i / 7 == 5 || i / 7 == 7) && (moves[i] + position) % 8 != 0)) {
+                        if ((tiles[moves[i] + position].getPiece() != null)) {
+                            if (tiles[moves[i] + position].getPiece().getPieceColor() != this.pieceColor) {
+                                possibleMoves.add(moves[i] + position);
+                            }
+                            i = i / 7 * 7 + 6;
+                        } else {
+                            possibleMoves.add(moves[i] + position);
+                        }
+                    } else {
+                        i = i / 7 * 7 + 6;
                     }
                 }
             }
